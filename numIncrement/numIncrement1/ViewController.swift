c4bb9b6d1fd0a2411bdd57aada8cd64ce1b78a58
asokol <asokol@udallas.edu>
2017-09-23 18:39:24 -0500

    Added first Project

---
+++ numIncrement/numIncrement1/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  numIncrement
+//
+//  Created by asokol on 9/21/17.
+//  Copyright © 2017 asokol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var subtractNum: UIButton!
+    @IBOutlet weak var addNum: UIButton!
+    var numStore: Int = 0  //Integer used to keep track of and update the String numLabel
+    
+    @IBAction func subtractOne(_ sender: Any) {
+        numStore = numStore - 1
+        if (numStore == 0){
+            numLabel.textColor = UIColor.black
+            }
+        else if (numStore < 0){
+            numLabel.textColor = UIColor.red
+        }
+        updateLabel()
+    }
+    
+    @IBAction func addOne(_ sender: Any) {
+        numStore = numStore + 1
+        if (numStore == 0){
+            numLabel.textColor = UIColor.black
+        }
+        else if (numStore > 0){
+            numLabel.textColor = UIColor.blue
+        }
+        updateLabel()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLabel()
+    }
+    
+    //Method that allows subtractOne and addOne to update numLabel when pressed
+    func updateLabel(){
+        numLabel.text = String(numStore)
+    }
+}
+
+
