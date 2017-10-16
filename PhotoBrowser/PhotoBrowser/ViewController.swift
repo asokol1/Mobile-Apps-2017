d911ca5a735c4b233af4f1a8e21b75a5d7d2da73
asokol <asokol@udallas.edu>
2017-10-16 18:02:00 -0500

    Added transitioning and fixed cell size

--- PhotoBrowser/PhotoBrowser/ViewController.swift
+++ PhotoBrowser/PhotoBrowser/ViewController.swift
@@ -20,14 +20,24 @@ class ViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
     }
-
+    
+    //Aloow for animated transitions between the two View Controllers
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segue"{
+            let photoViewController = segue.destination as? PhotoViewController
+            let collectionCell = sender as! ImageCell
+            photoViewController?.image = collectionCell.photo
+            
+ 
+            }
+        }
     func fetchData() {
         let url = URL(string: "https://api.flickr.com/services/rest/?format=json&sort=random&method=flickr.photos.search&tags=daffodil&tag_mode=all&api_key=0e2b6aaf8a6901c264acb91f151a3350&nojsoncallback=1")
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url!) { (data, response, err) in
             let data = data!
             let json = try! JSONSerialization.jsonObject(with: data, options: [])
-            //data required is an array in a dictionary in a dictionary
+            //data required are dictionaries in an array in a dictionary in a dictionary
             let outerDict = json as! [String:Any]
             let innerDict = outerDict["photos"] as! [String:Any]
             let array = innerDict["photo"] as! [[String: Any]]
